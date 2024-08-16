d2d8b3ceb2668eb905ca90697ee30eda611d4bea
José Raimundo <70080687+JoseRaimundo27@users.noreply.github.com>
2024-08-16 11:50:15 -0300

    Update lista4.sql

--- lista4.sql
+++ lista4.sql
@@ -11,3 +11,15 @@ INNER JOIN Fornecedor f
 ON  f.codFornecedor = c.codFornecedor
 WHERE f.nomeFornecedor = 'Empresa A'
 --Selecione AS COMIDAS QUE TÊM FORNECEDOR, e esse fornecedor tenha nome Empresa A
+--3) Selecionar os produtos que possuem mais de uma porção de comida.
+SELECT * FROM Comida c INNER JOIN ItensRefeicao i ON c.codProduto = i.codProduto
+--4) Selecione os nomes dos funcionários que comem mais de uma porção de comida.
+SELECT  f.nomefuncionario FROM Funcionario f INNER JOIN Refeicao r ON f.codFuncionario = r.codFuncionario
+INNER JOIN ItensRefeicao ir on r.codrefeicao = ir.codrefeicao where ir.quantidade > 1
+--5) Selecione os produtos que a funcionaria “Daniela” deseja comer.
+SELECT c.descProduto
+FROM Funcionario f
+INNER JOIN Refeicao r ON f.codFuncionario = r.codFuncionario
+INNER JOIN ItensRefeicao ir ON r.codRefeicao = ir.codRefeicao
+INNER JOIN Comida c ON ir.codProduto = c.codProduto AND ir.codFornecedor = c.codFornecedor
+WHERE f.nomeFuncionario = 'Daniela';
